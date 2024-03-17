e16134c934aff70d357fa006af0fe9db5d8e583a
Saurabh Saraswat <saurabh45215@gmail.com>
2024-03-17 14:19:21 +0000

    stage 7

--- script.sh
+++ script.sh
@@ -1,2 +1,6 @@
-git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git && docker compose up --build
-git clone https://github.com/ss0809/MusicPlayer.git && cd MusicPlayer && docker compose up --build
+git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git \
+&& git clone https://github.com/ss0809/MusicPlayer.git \ 
+&& git clone https://github.com/ss0809/core.git \ 
+&& docker compose up --build
+
+#requires .env and json in core
