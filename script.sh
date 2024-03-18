e8e9a3c0c663eb69a775c9a58de817697ab75d20
Saurabh Saraswat <saurabh45215@gmail.com>
2024-03-18 18:49:04 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git && git clone https://github.com/ss0809/MusicPlayer.git && git clone https://github.com/ss0809/core.git && docker-compose up --build
+git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git && git clone https://github.com/ss0809/MusicPlayer.git && git clone https://github.com/ss0809/core.git && docker-compose up --build && docker system prune -a
 #requires .env and json in core
