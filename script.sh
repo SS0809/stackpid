331000f802ab9a77b973a28b6d1264f0fc56b4a9
Saurabh Saraswat <saurabh45215@gmail.com>
2024-03-26 19:00:19 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,11 @@
 sudo yum update -y && sudo yum install -y docker && sudo service docker start && sudo usermod -a -G docker ec2-user 
 
+
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+
+
+
 git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git && git clone https://github.com/ss0809/MusicPlayer.git && git clone https://github.com/ss0809/core.git && docker-compose up --build && docker system prune -a
 #requires .env and json in core
