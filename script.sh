ceb3fefb7d7744a7dfa97ef747476cbe223b2b01
Saurabh Saraswat <saurabh45215@gmail.com>
2024-03-26 18:55:33 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,4 @@
+sudo yum update -y && sudo yum install -y docker && sudo service docker start && sudo usermod -a -G docker ec2-user 
+
 git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git && git clone https://github.com/ss0809/MusicPlayer.git && git clone https://github.com/ss0809/core.git && docker-compose up --build && docker system prune -a
 #requires .env and json in core
