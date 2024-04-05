b2f1cfb9243e05684640e91adc666749b10dbe37
Saurabh Saraswat <saurabh45215@gmail.com>
2024-04-05 11:03:41 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ sudo yum update -y && sudo yum install -y docker && sudo service docker start &&
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
-
+yum install git -y 
 
 
 git clone --single-branch --branch dev https://github.com/ss0809/PortfolioBuilder.git && git clone https://github.com/ss0809/MusicPlayer.git && git clone https://github.com/ss0809/core.git && docker-compose up --build && docker system prune -a
